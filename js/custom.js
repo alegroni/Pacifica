92b6f21ccbf5e5798f3396f9ae5ec9e0ad984de5
alegroni <ajmarzano@gmail.com>
2019-01-20 18:39:22 -0300

    Prueba de autoplay en mobile

--- js/custom.js
+++ js/custom.js
@@ -90,6 +90,27 @@ jQuery(document).ready(function($) {
 			// }
 		}
 	});
+
+	var video = document.querySelector('#fullscreen-slider video');
+
+	var promise = video.play();
+
+	// promise won’t be defined in browsers that don't support promisified play()
+	if (promise === undefined) {
+	  console.log('Promisified video play() not supported');
+	} else {
+	  promise.then(function() {
+	    console.log('Video playback successfully initiated, returning a promise');
+	  }).catch(function(error) {
+	    console.log('Error initiating video playback: ', error);
+	  });
+	}
+
+	video.onloadedmetadata = function() {
+	  var fileName = this.currentSrc.replace(/^.*[\\/]/, '');
+	  document.querySelector('#videoSrc').innerHTML = 'Playing video: ' + fileName;
+	};
+
 	// Bind external navigation arrows for fullscreen slider
 	$('#nav-arrows .nav-left').on('click', function(e){
 		e.preventDefault();
