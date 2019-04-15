93a4385ee0a74394ca065710c067861c853621cc
alemarzano <ajmarzano@gmail.com>
2019-04-15 10:14:31 -0300

    prueba scroll iOS

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,21 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
 	'use strict';
 
-/*==========================================================*/
-/* Preloader
-/*==========================================================*/
+	/*==========================================================*/
+	/* Preloader
+	/*==========================================================*/
 
 	// $(window).on('load', function(){
 	// 	$('#status').fadeOut();
 	// 	$('#preloader').delay(350).fadeOut('slow');
 	// });
 
-/*==========================================================*/
-/* Collapsible sidebar
-/*==========================================================*/
+	/*==========================================================*/
+	/* Collapsible sidebar
+	/*==========================================================*/
 
-	$('#sidebar-button, #overlay').click(function() {
+	$('#sidebar-button, #overlay').click(function () {
 		$('.portfolio-full').removeClass('portfolio-open');
 		$('#top').removeClass('portfolio-open');
 		$('#sidebar-button').toggleClass('open');
@@ -23,39 +23,39 @@ jQuery(document).ready(function($) {
 		return false;
 	});
 
-/*==========================================================*/
-/* Main menu
-/*==========================================================*/
+	/*==========================================================*/
+	/* Main menu
+	/*==========================================================*/
 
-	$('#mainmenu ul > li:has(ul)').each(function() {
+	$('#mainmenu ul > li:has(ul)').each(function () {
 		$(this).addClass('expandable');
 	});
 
-	$('#mainmenu ul > li:has(ul) > a').click(function() {
+	$('#mainmenu ul > li:has(ul) > a').click(function () {
 		$(this).parent('li').toggleClass('expanded');
 		$(this).parent('li').children('ul').slideToggle();
 		return false;
 	});
 
-/*==========================================================*/
-/* Swiper slider
-/*==========================================================*/
+	/*==========================================================*/
+	/* Swiper slider
+	/*==========================================================*/
 
 	/* Initialize sliders */
 
 	var swiper = [];
-	$('.swiper').each(function(i,obj){
+	$('.swiper').each(function (i, obj) {
 		swiper[i] = new Swiper(obj, {
 			loop: false,
 			calculateHeight: true
 
 		});
 		// Bind navigation arrows
-		$(this).children('.nav-left').on('click', function(e){
+		$(this).children('.nav-left').on('click', function (e) {
 			e.preventDefault();
 			swiper[i].swipePrev();
 		});
-		$(this).children('.nav-right').on('click', function(e){
+		$(this).children('.nav-right').on('click', function (e) {
 			e.preventDefault();
 			swiper[i].swipeNext();
 		});
@@ -63,19 +63,21 @@ jQuery(document).ready(function($) {
 
 	/* Resize fix for IE11 */
 
-	$(window).on('load', function(){
-		swiper.forEach(function(entry) {
-			window.setTimeout(function() { entry.resizeFix(); }, 500);
+	$(window).on('load', function () {
+		swiper.forEach(function (entry) {
+			window.setTimeout(function () {
+				entry.resizeFix();
+			}, 500);
 		});
 	});
 
 	/* Fullscreen slider */
 
-	var fSwiper = new Swiper('#fullscreen-slider',{
-		loop:true,
-		autoplay:5000,
+	var fSwiper = new Swiper('#fullscreen-slider', {
+		loop: true,
+		autoplay: 5000,
 
-		onSlideChangeStart: function() {
+		onSlideChangeStart: function () {
 
 			// Hide arrow on first and last slide
 			if (fSwiper.activeIndex == 0) {
@@ -94,14 +96,13 @@ jQuery(document).ready(function($) {
 
 			// promise won’t be defined in browsers that don't support promisified play()
 			if (promise === undefined) {
-					console.log('Promisified video play() not supported');
-				}
-				else {
-					promise.then(function() {
-							console.log('Video playback successfully initiated, returning a promise');
-					}).catch(function(error) {
-							console.log('Error initiating video playback: ', error);
-					});
+				console.log('Promisified video play() not supported');
+			} else {
+				promise.then(function () {
+					console.log('Video playback successfully initiated, returning a promise');
+				}).catch(function (error) {
+					console.log('Error initiating video playback: ', error);
+				});
 			}
 		}
 
@@ -111,25 +112,25 @@ jQuery(document).ready(function($) {
 
 
 	// Bind external navigation arrows for fullscreen slider
-	$('#nav-arrows .nav-left').on('click', function(e){
+	$('#nav-arrows .nav-left').on('click', function (e) {
 		e.preventDefault();
 		fSwiper.swipePrev();
 	});
-	$('#nav-arrows .nav-right').on('click', function(e){
+	$('#nav-arrows .nav-right').on('click', function (e) {
 		e.preventDefault();
 		fSwiper.swipeNext();
 	});
 	// Resize videos in fullscreen slider
 	function resizeToCover() {
-		$('#fullscreen-slider .swiper-slide').each(function() {
+		$('#fullscreen-slider .swiper-slide').each(function () {
 			if ($(this).has('video').length) {
 				var vid_w_orig = parseInt($(this).find('video').attr('width'));
 				var vid_h_orig = parseInt($(this).find('video').attr('height'));
 				var container_w = $(this).width();
 				var container_h = $(this).height();
 				// Use largest scale factor of horizontal / vertical
-				var scale_h =  container_w / vid_w_orig;
-				var scale_v =  container_h / vid_h_orig;
+				var scale_h = container_w / vid_w_orig;
+				var scale_v = container_h / vid_h_orig;
 				var scale = scale_h > scale_v ? scale_h : scale_v;
 				// Scale the video
 				$(this).find('video').width(scale * vid_w_orig);
@@ -141,16 +142,17 @@ jQuery(document).ready(function($) {
 		});
 	}
 	resizeToCover();
+
 	function resizeToCoverSnap() {
-		$('.snap').each(function() {
+		$('.snap').each(function () {
 			if ($(this).has('video').length) {
 				var vid_w_orig = parseInt($(this).find('video').attr('width'));
 				var vid_h_orig = parseInt($(this).find('video').attr('height'));
 				var container_w = $(this).width();
 				var container_h = $(this).height();
 				// Use largest scale factor of horizontal / vertical
-				var scale_h =  container_w / vid_w_orig;
-				var scale_v =  container_h / vid_h_orig;
+				var scale_h = container_w / vid_w_orig;
+				var scale_v = container_h / vid_h_orig;
 				var scale = scale_h > scale_v ? scale_h : scale_v;
 				// Scale the video
 				$(this).find('video').width(scale * vid_w_orig);
@@ -163,25 +165,25 @@ jQuery(document).ready(function($) {
 	}
 	resizeToCoverSnap();
 
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	 $("#wave").attr('poster', 'img/poster_waves.jpg');
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		$("#wave").attr('poster', 'img/poster_waves.jpg');
 		$("#casa").attr('poster', 'img/poster_casa.jpg');
 	};
 
 
-/*==========================================================*/
-/* On resize
-/*==========================================================*/
+	/*==========================================================*/
+	/* On resize
+	/*==========================================================*/
 
-	$(window).resize(function() {
+	$(window).resize(function () {
 		resizeToCover();
 	});
 
-/*==========================================================*/
-/* On scroll
-/*==========================================================*/
+	/*==========================================================*/
+	/* On scroll
+	/*==========================================================*/
 
-	$('#content').scroll(function(){
+	$('#content').scroll(function () {
 		// Add shadow to top header
 		if ($('#content').scrollTop() > 0) {
 			$('#top').addClass('shadow');
@@ -191,9 +193,9 @@ jQuery(document).ready(function($) {
 	});
 
 
-/*==========================================================*/
-/* Portfolio Item
-/*==========================================================*/
+	/*==========================================================*/
+	/* Portfolio Item
+	/*==========================================================*/
 
 	// $('article.portfolio a').click(function() {
 	// 	var itemID = $(this).attr('href');
@@ -207,12 +209,12 @@ jQuery(document).ready(function($) {
 	// 	return false;
 	// });
 
-/*==========================================================*/
-/* AJAX Contact form
-/*==========================================================*/
+	/*==========================================================*/
+	/* AJAX Contact form
+	/*==========================================================*/
 
-	$('#contact-form').submit(function() {
-		$.post('send.php', $(this).serialize(), function(data){
+	$('#contact-form').submit(function () {
+		$.post('send.php', $(this).serialize(), function (data) {
 			$('#contact-form').html('<p>' + data + '</p>');
 		});
 		return false;
@@ -225,18 +227,18 @@ jQuery(document).ready(function($) {
 /* Swiper slider
 /*==========================================================*/
 
-	$('.gallery').justifiedGallery({
-		lastRow: 'nojustify',
-		rowHeight: 250,
-		rel: 'gallery1', //replace with 'gallery1' the rel attribute of each link
-		margins: 10
-	}).on('jg.complete', function () {
-		$(this).find('a').colorbox({
-			maxWidth: '100%',
-			maxHeight: '100%',
-			opacity: 0.7,
-			transition: 'elastic',
-			current: ''
-		});
+$('.gallery').justifiedGallery({
+	lastRow: 'nojustify',
+	rowHeight: 250,
+	maxRowHeight: 250,
+	// rel: 'gallery1', //replace with 'gallery1' the rel attribute of each link
+	margins: 10
+}).on('jg.complete', function () {
+	$(this).find('a').colorbox({
+		maxWidth: '100%',
+		maxHeight: '100%',
+		opacity: 0.7,
+		transition: 'elastic',
+		current: ''
 	});
-
+});
